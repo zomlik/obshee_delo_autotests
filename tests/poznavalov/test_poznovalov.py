6cd3792bcaa99651bc52883f7cdde0c58c4a3e15
Zomlik <sanek77594@yandex.ru>
2023-12-08 10:00:46 +0300

    add test one time support

--- tests/poznavalov/test_poznovalov.py
+++ tests/poznavalov/test_poznovalov.py
@@ -1,4 +1,7 @@
+import pytest
+
 from pages.poznavalov.main_page import MainPage
+from locators.poznovalov_locators import MainPageLocators
 from data.fake_date import FakeDate
 import time
 
@@ -16,4 +19,18 @@ class TestPayment(FakeDate):
         page.phone_field(self.phone())
         page.click_donate_button()
         page.swith_pay_form_iframe()
-        page.click_pay_button()
+        assert page.click_pay_button()
+
+    def test_one_time_support(self, browser):
+        page = MainPage(browser)
+        page.open(URL)
+        page.click_support_button()
+        page.click_once_time_button()
+        page.sum_field(self.amount(min_value=10, max_value=30000))
+        page.email_field(self.email())
+        page.name_field(self.name())
+        page.phone_field(self.phone())
+        page.click_donate_button()
+        page.swith_pay_form_iframe()
+        assert page.click_pay_button()
+
