eb08f19f977a6ec10e65d12e1ef3053a61d1721e
Zomlik <sanek77594@yandex.ru>
2023-12-11 10:17:30 +0300

    fix data

--- data/fake_data.py
+++ data/fake_data.py
@@ -3,19 +3,21 @@ from faker import Faker
 
 
 class FakeData:
+    LOCALIZATION = "ru_RU"
+
     @staticmethod
     def amount(min_value: int, max_value: int):
         return randint(min_value, max_value)
 
     @property
     def name(self):
-        return Faker("ru_RU").name
+        return Faker(self.LOCALIZATION).name
 
     @property
     def phone(self):
-        return Faker("ru_RU").phone_number
+        return Faker(self.LOCALIZATION).phone_number
 
     @property
     def email(self):
-        return Faker("ru_RU").email
+        return Faker(self.LOCALIZATION).email
 
