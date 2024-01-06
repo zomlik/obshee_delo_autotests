751026587a9f6e8b45b06dc971726cae47e37ff6
Zomlik <sanek77594@yandex.ru>
2024-01-06 07:01:21 +0300

    fix

--- locators/donation_obshee_delo_locators.py
+++ locators/donation_obshee_delo_locators.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
+    """Главное меню Donation Obshee-Delo"""
+    MENU_SUPPORT_BUTTON = (By.CSS_SELECTOR, ".header-support__btn")
+
     """Форма оплаты Donation Obshee-Delo"""
     SUM_FIELD = (By.CSS_SELECTOR, ".donate_amount_flex")
     NAME_FIELD = (By.XPATH, "//input[@name='leyka_donor_name']")
