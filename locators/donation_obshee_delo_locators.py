4c0ea3ea31581801d0cea2de87705df8354c8116
Zomlik <sanek77594@yandex.ru>
2024-01-06 07:00:28 +0300

    fix

--- locators/donation_obshee_delo_locators.py
+++ locators/donation_obshee_delo_locators.py
@@ -2,14 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    """Главное меню Donation Obshee-Delo"""
-    MENU_MAIN = (By.CSS_SELECTOR, "#u84-4")
-    MENU_SUPPORT_NEW_CARTON = (By.CSS_SELECTOR, "#u98-6")
-    MENU_COST_PROJECT = (By.CSS_SELECTOR, "#u89-4")
-    MENU_ABOUT = (By.CSS_SELECTOR, "#u104-6")
-    MENU_SUPPORT_BUTTON = (By.CSS_SELECTOR, ".header-support__btn")
-    LIST_OF_MENU_BUTTONS = [MENU_MAIN, MENU_SUPPORT_NEW_CARTON, MENU_COST_PROJECT, MENU_ABOUT]
-
     """Форма оплаты Donation Obshee-Delo"""
     SUM_FIELD = (By.CSS_SELECTOR, ".donate_amount_flex")
     NAME_FIELD = (By.XPATH, "//input[@name='leyka_donor_name']")
